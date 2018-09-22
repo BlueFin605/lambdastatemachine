648be40d58dc02f6d174d1ef56a32f686096c4ad
BlueFin605 <BlueFin605@gmail.com>
2018-09-22 17:13:57 +1200

    remove intermeditry callback

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 var sm = require('./statemachine/statemachine')
 
 exports.offhook = function (event, context, callback) {
-    sm.StateMachine().changeStateAsync((state, data, changecallback) => state.offHook(event, changecallback), (err, result) => onCompleted(err, result, context, callback))
+    sm.StateMachine().changeStateAsync((state, data, changecallback) => state.offHook(event, changecallback), callback)
 }
 
 exports.dial = function (event, context, callback) {
-    sm.StateMachine().changeStateAsync((state, data, changecallback) => state.dial(event, changecallback), (err, result) => onCompleted(err, result, context, callback))
+    sm.StateMachine().changeStateAsync((state, data, changecallback) => state.dial(event, changecallback), callback)
 }
 
 exports.connected = function (event, context, callback) {
-    sm.StateMachine().changeStateAsync((state, data, changecallback) => state.connected(event, changecallback), (err, result) => onCompleted(err, result, context, callback))
+    sm.StateMachine().changeStateAsync((state, data, changecallback) => state.connected(event, changecallback), callback)
 }
 
 exports.hangup = function (event, context, callback) {
-    sm.StateMachine().changeStateAsync((state, data, changecallback) => state.hangUp(event, changecallback), (err, result) => onCompleted(err, result, context, callback))
+    sm.StateMachine().changeStateAsync((state, data, changecallback) => state.hangUp(event, changecallback), callback)
 }
 
-function onCompleted(err, result, context, callback) {
-    console.log(`onCompleted(${err},${result})`)
-    callback(err,result)
-}
+// function onCompleted(err, result, context, callback) {
+//     console.log(`onCompleted(${err},${result})`)
+//     callback(err,result)
+// }
+
 // this.offhook('event offhook', null, (err, result) => console.log(`response from transition<${err}>`))
 // this.dial('event dial', null, (err, result) => console.log(`response from transition<${err}>`))
 // this.connected('event connected', null, (err, result) => console.log(`response from transition<${err}>`))
