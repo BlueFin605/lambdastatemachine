a2a54be0c148eabb88c0a7aa886dc25dce9de389
BlueFin605 <BlueFin605@gmail.com>
2018-09-22 12:30:33 +1200

    pass in the aws callback rather than implementing my own

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 var sm = require('./statemachine/statemachine')
 
 exports.offhook = function (event, context, callback) {
-    sm.StateMachine.changeStateAsync((state, data, callback) => state.offHook(event, callback), (err, result) => console.log(`response from transition<${err}>`))
+    sm.StateMachine.changeStateAsync((state, data, changecallback) => state.offHook(event, changecallback), callback)
 }
 
 exports.dial = function (event, context, callback) {
-    sm.StateMachine.changeStateAsync((state, data, callback) => state.dial(event, callback), (err, result) => console.log(`response from transition<${err}>`))
+    sm.StateMachine.changeStateAsync((state, data, changecallback) => state.dial(event, changecallback), callback)
 }
 
 exports.connected = function (event, context, callback) {
-    sm.StateMachine.changeStateAsync((state, data, callback) => state.connected(event, callback), (err, result) => console.log(`response from transition<${err}>`))
+    sm.StateMachine.changeStateAsync((state, data, changecallback) => state.connected(event, changecallback), callback)
 }
 
 exports.hangup = function (event, context, callback) {
-    sm.StateMachine.changeStateAsync((state, data, callback) => state.hangUp(event, callback), (err, result) => console.log(`response from transition<${err}>`))
+    sm.StateMachine.changeStateAsync((state, data, changecallback) => state.hangUp(event, changecallback), callback)
 }
 
-// this.offhook('event offhook', null, null)
-// this.dial('event dial', null, null)
-// this.connected('event connected', null, null)
-// this.hangup('event hangup', null, null)
+// this.offhook('event offhook', null, (err, result) => console.log(`response from transition<${err}>`))
+// this.dial('event dial', null, (err, result) => console.log(`response from transition<${err}>`))
+// this.connected('event connected', null, (err, result) => console.log(`response from transition<${err}>`))
+// this.hangup('event hangup', null, (err, result) => console.log(`response from transition<${err}>`))
